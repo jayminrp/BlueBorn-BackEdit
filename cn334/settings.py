539f04c43fb5123aa2edbdfa377778197ad5957d
jayminrp <rapeephanwadee.plo@dome.tu.ac.th>
2025-05-15 01:02:55 +0700

    Edit To connect front

--- cn334/settings.py
+++ cn334/settings.py
@@ -64,6 +64,31 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://front-blueborn-cyioy3vfg-jaymins-projects-7667260e.vercel.app",
+    "https://front-blueborn.vercel.app",
+]
+
+# Add additional CORS settings
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 REST_FRAMEWORK = {
