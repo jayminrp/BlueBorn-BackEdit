a151dcc11fdbfd84cd3523047332e9f9fe1217e2
jayminrp <rapeephanwadee.plo@dome.tu.ac.th>
2025-05-14 19:12:32 +0700

    อีกทีลูก

--- cn334/settings.py
+++ cn334/settings.py
@@ -102,7 +102,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "cn334.wsgi.application"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -153,9 +152,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICIFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
