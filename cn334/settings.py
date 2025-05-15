759e5626fd42ed207b1ccdeed1f2ee4d763f9c18
jayminrp <rapeephanwadee.plo@dome.tu.ac.th>
2025-05-15 19:16:36 +0700

    edit on render site url

--- cn334/settings.py
+++ cn334/settings.py
@@ -69,7 +69,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://front-blueborn-pgzfrg705-jaymins-projects-7667260e.vercel.app",
     "https://front-bb.vercel.app",
     "https://front-blueborn-n2dmjrauu-jaymins-projects-7667260e.vercel.app",
-    "https://front-bb.onrender.com/",
+    "https://front-bb.onrender.com",
 ]
 
 # Add additional CORS settings
